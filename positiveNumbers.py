c01a755ef03a37fe6cfa11d55314381c4edebb93
+ <+>
2023-12-10 01:15:06 +0300

    fixed funcion caling error

--- positiveNumbers.py
+++ positiveNumbers.py
@@ -1,14 +1,15 @@
-def two_positive_numbers (a, b, c):
+def two_positive_numbers(a, b, c):
+    #checks whether only 2 argurments are positve
     if a > 0 and b > 0 and c > 0:
-        print ("false")
+        print("false")
     elif a > 0 and b > 0 and c < 0:
-        print ("true")
+        print("true")
     elif a > 0 and b < 0 and c > 0:
-        print ("true")
-    elif a < 0 and b > 0 and c >0:
-        print ("true")
+        print("true")
+    elif a < 0 and b > 0 and c > 0:
+        print("true")
     elif a < 0 and b < 0 and c < 0:
-        print ("false")
-    
+        print("false")
 
-    print (two_positive_numbers(1, -2, 3))
+
+two_positive_numbers(1, -2, 3)
